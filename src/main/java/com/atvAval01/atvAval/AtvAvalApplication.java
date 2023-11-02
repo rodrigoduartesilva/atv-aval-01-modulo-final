6b8d314fa383ca1f41e344697352bc1518c9aab9
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-11-02 15:08:01 -0300

    Feat: First Commit.

---
+++ src/main/java/com/atvAval01/atvAval/AtvAvalApplication.java
@@ -0,0 +1,13 @@
+package com.atvAval01.atvAval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvAvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvAvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atvAval01/atvAval/AtvAvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atvAval01.atvAval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvAvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
